551314f07dd6f91c5223bd02baf889ac6593bb4f
TobiasTheDanish <t.hc199811@gmail.com>
2022-09-29 10:52:53 +0200

    Task 3 Done

--- Uge 4/Task2/src/Main.java
+++ Uge 4/Task2/src/Main.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
+
 public class Main
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please type your name: ");
+        String name = input.nextLine();
+
+        System.out.println("Name: " + name);
+
+        System.out.print("Please type your age: ");
+        int age = input.nextInt();
+
+        System.out.println("Age: " + age);
+
+        int result = 67 - age;
+        System.out.println("You can retire in: " + result + " years.");
     }
 }
